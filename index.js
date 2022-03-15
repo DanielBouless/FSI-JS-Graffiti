f5c34d47d2803a64730ae50a3ab9be1f414adf55
Daniel Bouless <dbouless76@gmail.com>
2022-03-15 19:32:39 -0400

    modified things

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Select some elements...
+let rotation = 0
+let blurLvl = 0
+let invertLvl = 0
+let pageHeader = document.querySelector('#page-header')
+
+pageHeader.style.textAlign = 'left'
+
+let dogImages = document.querySelectorAll('.dog-image')
+
+for(let i=0; i< dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    let rotationMulti = i*90
+    rotation = 'rotate'+'('+rotationMulti.toString()+'deg)'
+    dogImages[i].style.transform = rotation
+    let blurMulti = i*3
+    blurLvl = 'blur'+'('+blurMulti.toString()+'px)'
+    dogImages[i].style.filter = blurLvl
+    let invertMulti = i*33
+    invertLvl = 'invert'+'('+invertMulti.toString()+'%)'
+    dogImages[i].style.filter = invertLvl
+}
+
+
